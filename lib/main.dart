81c1715fe1aef10a08c3855e3675c06427776155
FilipKostic1999 <kosticfilip99@gmail.com>
2023-11-10 15:27:26 +0100

    c

--- lib/main.dart
+++ lib/main.dart
@@ -30,14 +30,17 @@ class _MyScreenState extends State<MyScreen> {
   }
 
   Future<void> fetchData() async {
+    // Fetch data from the API
     final response = await http.get(Uri.parse(
         'https://api.sr.se/api/v2/scheduledepisodes?channelid=164&format=json'));
 
     if (response.statusCode == 200) {
+      // Update the state with the fetched schedule data
       setState(() {
         scheduleData = json.decode(response.body)['schedule'];
       });
     } else {
+      // Handle the case when data fetching fails
       throw Exception('Failed to load data');
     }
   }
@@ -57,39 +60,96 @@ class _MyScreenState extends State<MyScreen> {
                 final item = scheduleData[index];
                 final imageUrl = item['imageurl'];
 
-                return Container(
-                  margin: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius:
-                        BorderRadius.circular(12), // Make corners round
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey,
-                        offset: Offset(0, 2),
-                        blurRadius: 6,
+                return GestureDetector(
+                  onTap: () {
+                    // Navigate to the detail screen when an item is tapped
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailScreen(item: item),
                       ),
-                    ],
-                  ),
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(12), // Make corners round
+                    );
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0, 2),
+                          blurRadius: 6,
+                        ),
+                      ],
                     ),
-                    child: ListTile(
-                      title: Text(item['title']),
-                      subtitle: Text(item['description']),
-                      leading: imageUrl != null
-                          ? Image.network(
-                              imageUrl,
-                              width: 60,
-                            )
-                          : SizedBox(width: 60),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: ListTile(
+                        title: Text(item['title']),
+                        subtitle: Text(item['description']),
+                        leading: imageUrl != null
+                            ? Image.network(
+                                imageUrl,
+                                width: 60,
+                              )
+                            : SizedBox(width: 60),
+                      ),
                     ),
                   ),
                 );
               },
             ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final dynamic item;
+
+  DetailScreen({required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl = item['imageurl'];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(item['title']),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Display the image if available
+            imageUrl != null
+                ? Image.network(
+                    imageUrl,
+                    width: double.infinity,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  )
+                : SizedBox.shrink(),
+            SizedBox(height: 16),
+            // Display the description
+            Text(
+              'Description:',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              item['description'],
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
